54fab77561cf0b8f267cf36ccbf012b4438c3514
dddd0ng <117462826+dddd0ng@users.noreply.github.com>
2025-08-15 23:41:15 +0900

    te

--- test-project/src/main/java/com/swcamp/test/Application.java
+++ test-project/src/main/java/com/swcamp/test/Application.java
@@ -2,6 +2,6 @@ package com.swcamp.test;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("test");
     }
 }
