58e7519c2e1b2b3e1d484dceb5addea5b65fc776
dddd0ng <kdg1644@naver.com>
2025-09-08 13:56:26 +0900

    st

--- 07_springboot/chap01-auto-configuration-lecture-source/src/main/java/com/swcamp/chap01/Chap01AutoConfigurationLectureSourceApplication.java
+++ 07_springboot/chap01-auto-configuration-lecture-source/src/main/java/com/swcamp/chap01/Chap01AutoConfigurationLectureSourceApplication.java
@@ -2,8 +2,8 @@ package com.swcamp.chap01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+//얜 그냥 부팅용
+@SpringBootApplication // 이 어노테이션이 핵심
 public class Chap01AutoConfigurationLectureSourceApplication {
 
     public static void main(String[] args) {
