59029e643bffe695c434d0fcbe99170eaa46390e
dddd0ng <kdg1644@naver.com>
2025-09-09 14:59:52 +0900

    st

--- 07_springboot/chap04-view-resolver-lecture-source/src/main/java/com/swcamp/viewresolver/MainController.java
+++ 07_springboot/chap04-view-resolver-lecture-source/src/main/java/com/swcamp/viewresolver/MainController.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //컨트롤러 어노테이션 해줘야 리퀘스트매핑 쓸수있음
 @Controller
 public class MainController {
-//    @RequestMapping("/") //
-//    public String defaultLocation(){
-//        return "main"; // classpath:/templates/ . html이 붙음  (접두사/접미사)
+//    @RequestMapping("/")
+//    public String defaultLocation() {
+//        return "main";
 //    }
+//
 //    @RequestMapping("/main")
-//    public void main(){}
+//    public void main() {}
 
     @RequestMapping(value={"/", "/main"})
     public String main() {return "main";}
