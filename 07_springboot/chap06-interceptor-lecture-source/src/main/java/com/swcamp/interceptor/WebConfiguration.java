4db2fa31986bf33f8c278e790bf745b86b07002c
dddd0ng <kdg1644@naver.com>
2025-09-10 12:48:35 +0900

    st

--- 07_springboot/chap06-interceptor-lecture-source/src/main/java/com/swcamp/interceptor/WebConfiguration.java
+++ 07_springboot/chap06-interceptor-lecture-source/src/main/java/com/swcamp/interceptor/WebConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /* 설명. WebMvcConfigurer 관련 설정 : Interceptor추가 및 static(정적)리소스 호출 경로 등록 설정*/
+//기본적으로 설정용 파일
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
     //bean이니까 의존성 주입가능
@@ -16,13 +17,14 @@ public class WebConfiguration implements WebMvcConfigurer {
         this.stopwatchInterceptor = stopwatchInterceptor;
     }
 
-
+/* 설명. 인터셉터 추가 및 동작 요청 패턴 설정*/
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(stopwatchInterceptor)
                 .excludePathPatterns("/css/**");
     }
 
+    /* 설명. 정적 리소스 호출을 위한 설정*/
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**") //정적 리소스 요청
@@ -30,5 +32,3 @@ public class WebConfiguration implements WebMvcConfigurer {
                 .setCachePeriod(100); //응답 속도 향상을 위한 캐싱 시간 설정
     }
 }
-
-//기본적으로 설정용 파일
