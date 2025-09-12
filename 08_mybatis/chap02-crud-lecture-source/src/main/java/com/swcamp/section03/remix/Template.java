e05b73769028eecf17a419ecbaaad3dd52466d88
dddd0ng <kdg1644@naver.com>
2025-09-12 14:02:46 +0900

    st

---
+++ 08_mybatis/chap02-crud-lecture-source/src/main/java/com/swcamp/section03/remix/Template.java
@@ -0,0 +1,28 @@
+package com.swcamp.section03.remix;
+
+public class Template {
+    private static String driver = "com.mysql.cj.jdbc.Driver";
+    private static String url = "jdbc:mysql://localhost:3306/menudb";
+    private static String user = "swcamp";
+    private static String password = "swcamp";
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    /* 설명. Lazy Singleton 방식으로 작성 */
+    public static SqlSession getSqlSession() {
+        if(sqlSessionFactory == null) {
+            Environment environment =
+                    new Environment("dev"
+                            , new JdbcTransactionFactory()
+                            , new PooledDataSource(driver, url, user, password));
+            Configuration configuration = new Configuration(environment);
+
+            /* 설명. Mapper용 인터페이스(feat. 쿼리 포함 인터페이스) 추가 */
+            configuration.addMapper(MenuMapper.class);
+
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+        }
+
+        return sqlSessionFactory.openSession(false);
+    }
+}
