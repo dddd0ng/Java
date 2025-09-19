2f1e88b7cef93592fdfb1b8528d50f87231c7631
dddd0ng <kdg1644@naver.com>
2025-09-19 10:46:46 +0900

    st

--- 10_jpa/chap05-spring-data-jpa-lecture-source/src/main/java/com/swcamp/springdatajpa/menu/service/MenuService.java
+++ 10_jpa/chap05-spring-data-jpa-lecture-source/src/main/java/com/swcamp/springdatajpa/menu/service/MenuService.java
@@ -1,18 +1,37 @@
 package com.swcamp.springdatajpa.menu.service;
 
 import com.swcamp.springdatajpa.menu.dto.MenuDTO;
+import com.swcamp.springdatajpa.menu.entity.Menu;
 import com.swcamp.springdatajpa.menu.repository.MenuRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 //(controller의 )의존성 주입 받아야함
 //    @Autowired
 //    public MenuController(MenuService menuService) {
 //        this.menuService = menuService;
 public class MenuService {
     private final MenuRepository menuRepository;
+//service 계층
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public MenuService(MenuRepository menuRepository
+                       , ModelMapper modelMapper) {
+        this.menuRepository = menuRepository;
+        this.modelMapper = modelMapper;
+    }
 
     public MenuDTO findMenuByCode(int menuCode) {
-        return null;
+        Menu menu = menuRepository.findById(menuCode)
+                .orElseThrow(IllegalArgumentException::new);
+
+        log.debug("service계층에서 하나의 메뉴 상세보기 : {} ", menu);
+
+        return modelMapper.map(menu, MenuDTO.class);
     }
 }
