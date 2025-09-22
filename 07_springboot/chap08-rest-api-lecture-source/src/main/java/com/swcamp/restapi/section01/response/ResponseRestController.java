3738450b4928b3cf7390600ee64c5e4f2f377db7
dddd0ng <kdg1644@naver.com>
2025-09-22 11:20:59 +0900

    st

--- 07_springboot/chap08-rest-api-lecture-source/src/main/java/com/swcamp/restapi/section01/response/ResponseRestController.java
+++ 07_springboot/chap08-rest-api-lecture-source/src/main/java/com/swcamp/restapi/section01/response/ResponseRestController.java
@@ -4,13 +4,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//@Controller
+@RestController //이 컨트롤러의 모든 핸들러 메소드의 응답은 view resolver를 활용하지 않는다.(SSR X)
 @RequestMapping("/response")
 public class ResponseRestController {
     @GetMapping("/hello")
-    @ResponseBody //이걸 붙인 순간부터는 view resolved로 안감
+//    @ResponseBody //이걸 붙인 순간부터는 view resolver로 안감
+    //핸들러 메소드에 응답이 json형태로 되며 자바의 모든 타입 반환이 가능해진다.
     public String helloWorld() {
         return "Hello World!";
     }
+
+    @GetMapping("/random")
+    public int getRandomNumber() {
+        return (int)(Math.random()*10)+1;
+    }
+
+    @GetMapping("/test")
+    public List<Map<String, Object>> getTest() {
+        List<Map<String,Object>> list = new ArrayList<>();
+        Map<String,Object> map = new HashMap<>();
+        map.put("test1",new Message(200,"성공1"));
+        map.put("test2",new Message(200,"성공2"));
+        map.put("MaxPageLink","http://localhost:8080/hello");
+        list.add(map);
+
+        return list;
+    }
+    // 객체형태, 배열형태
 }
