f78c209c88221ce0f311194060465aa6a8b51eb3
dddd0ng <kdg1644@naver.com>
2025-09-25 10:45:33 +0900

    st

--- 11_msa/chap04-02-order-service-lecture-source/src/main/java/com/swcamp/orderservice/controller/OrderController.java
+++ 11_msa/chap04-02-order-service-lecture-source/src/main/java/com/swcamp/orderservice/controller/OrderController.java
@@ -1,8 +1,16 @@
 package com.swcamp.orderservice.controller;
 
+import com.swcamp.orderservice.dto.OrderDTO;
+import com.swcamp.orderservice.dto.ResponseOrderDTO;
 import com.swcamp.orderservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -11,5 +19,29 @@ public class OrderController {
     @Autowired
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
+    }
+
+    //resultMap에 반환하는애들???????????
+    @GetMapping("/users/{userId}/orders")
+    public ResponseEntity<List<ResponseOrderDTO>> getUserOrders(@PathVariable int userId){
+     List<OrderDTO> orderDTOList = orderService.getOrderByUserId(userId);
+     List<ResponseOrderDTO> returnValue = orderDTOToResponseOrder(orderDTOList);
+    return ResponseEntity.ok().body(returnValue);
+    }
+
+    private List<ResponseOrderDTO> orderDTOToResponseOrder(List<OrderDTO> orderDTOList) {
+
+        List<ResponseOrderDTO> responseList = new ArrayList<>();
+
+        for (OrderDTO orderDTO : orderDTOList) {
+            ResponseOrderDTO responseOrder = new ResponseOrderDTO();
+            responseOrder.setOrderDate(orderDTO.getOrderDate());
+            responseOrder.setOrderTime(orderDTO.getOrderTime());
+            responseOrder.setOrderMenus(orderDTO.getOrderMenus());
+
+            responseList.add(responseOrder);
+        }
+
+        return responseList;
     }
 }
