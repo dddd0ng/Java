3795c6b0b93ab0aa165fe477a82cd38c3ce18af1
dddd0ng <kdg1644@naver.com>
2025-10-23 11:43:22 +0900

    st

---
+++ 15_vue/07_slot/src/App.vue
@@ -0,0 +1,29 @@
+<!-- 
+다른 SFC(single file component) (다른 파일)을 불러올 때는
+상대경로(./) 또는 src부터의 경로인 절대경로(@)를 사용할 수 있다. 
+-->
+
+<script setup>
+// import MyComponents from './components/01_component/MyComponents.vue';
+import MyComponents from '@/components/01_component/MyComponents.vue'; 
+import SlotParent from './components/02_slotIntro/SlotParent.vue';
+
+</script>
+
+<template>
+<header>
+  </header>
+<div class="redColor">
+  <MyComponents/>
+</div>
+  <main>
+    <SlotParent/>
+    </main>
+
+</template>
+
+<style scoped>
+.redColor{
+  color:red;
+}
+</style>
---
+++ 15_vue/07_slot/src/components/01_component/MyComponents.vue
@@ -0,0 +1,35 @@
+<!-- 
+플러그인 설치 이후 vbase-3-setup로 작성
+-->
+
+<!-- 
+template (템플릿 영역이라고 함)
+vue는 사용자의 화면에 보여질 부분을 template 영역에 html요소들로 작성한다.
+(template 태그는 DOM tree로 구성되지 않고 렌더링 될 때 해석되지 않는다.)
+-->
+<template>
+    <div>
+<h1 id="test">MyComponent</h1>
+    </div>
+</template>
+
+<script setup>
+ import {onMounted} from 'vue';
+
+ onMounted (()=>{
+console.log ('MyComponent 부착!');
+ })
+</script>
+
+
+<!-- 
+style scoped
+현재 컴포넌트에서만 적용되는 스타일을 작성한다.(범위 css)
+(해당 컴포넌트가 또 다른 컴포는트를 품고 있으면 하위 컴포넌트에도 적용되는 css)
+-->
+<style scoped>
+#test{
+    color:blue;
+}
+</style>
+
