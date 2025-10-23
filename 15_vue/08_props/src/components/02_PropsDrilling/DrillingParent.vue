38d4d841e1f5190481ff7e75eca2669e107015b4
dddd0ng <kdg1644@naver.com>
2025-10-23 15:13:20 +0900

    st

---
+++ 15_vue/08_props/src/components/02_PropsDrilling/DrillingParent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Parent : {{ count }}</h1>
+        <DrillingChild />
+    </div>
+</template>
+
+<script setup>
+    import DrillingChild from './DrillingChild.vue';
+    import {ref, provide, readonly} from 'vue';
+    const count = ref(2);
+    // provide('count', count);
+    provide('count', readonly(count));
+
+</script>
+
+<style scoped>
+
+</style>
