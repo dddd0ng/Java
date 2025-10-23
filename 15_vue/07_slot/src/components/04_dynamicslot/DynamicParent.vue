e71156d74a572358b7ab0ea5ddb2bda3512bd59b
dddd0ng <kdg1644@naver.com>
2025-10-23 12:20:19 +0900

    st

---
+++ 15_vue/07_slot/src/components/04_dynamicslot/DynamicParent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="parentContainer">
+<input type="text" v-model="dynamicName">
+<DynamicChild>
+    <template #[dynamicName]>
+{{ dynamicName }}
+
+    </template>
+    </DynamicChild>
+    </div>
+</template>
+
+<script setup>
+import DynamicChild from './DynamicChild.vue';
+import {ref} from 'vue';
+
+const dynamicName = ref('');
+</script>
+
+<style scoped>
+
+    .parentContainer {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        border: 1px solid black;
+    }
+</style>
