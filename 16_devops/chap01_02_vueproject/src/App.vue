9b09624213cdef54397876ed14287802b27eae21
dddd0ng <kdg1644@naver.com>
2025-11-05 11:50:51 +0900

    st

--- 16_devops/chap01_02_vueproject/src/App.vue
+++ 16_devops/chap01_02_vueproject/src/App.vue
@@ -54,15 +54,24 @@
     //   body: JSON.stringify({num1: num1.value, num2: num2.value})
     // })
 
-    // 
-        const response = await fetch(`http://localhost:30001/plus`, {
+    // 6. 백엔드에서 CORS, 프론트에서 X (k8s를 활용한 nodeport 방식)
+    //     const response = await fetch(`http://localhost:30001/plus`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json;charset=utf-8;'
+    //   },
+    //   body: JSON.stringify({num1: num1.value, num2: num2.value})
+    // })
+    
+// 7. 백엔드에서 x, 프론트에서도 x (k8s의 ingress를 활용한 방식)
+    // const response = await fetch(`http://localhost:80/boot/plus`, { 이거랑 아래랑 같음
+    const response = await fetch(`http://localhost/boot/plus`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json;charset=utf-8;'
       },
       body: JSON.stringify({num1: num1.value, num2: num2.value})
     })
-    
     
     const data = await response.json();
     sum.value = data.sum;
